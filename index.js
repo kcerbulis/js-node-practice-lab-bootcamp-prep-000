952038a6871fabc07ace4a2ca9cd3596840aebfc
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-05 01:46:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Krisjanis";
+var name = "Joe";
 var height = 19;
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
